9009aa829181a4ed2d2d2bfaf38292326557a643
Alec Wixom <atwixom@gmail.com>
2023-05-10 09:59:16 -0600

    initial upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let phoneNumber = function (numbers) {
+    return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6).join('')}`
+}
+
+console.log(phoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
