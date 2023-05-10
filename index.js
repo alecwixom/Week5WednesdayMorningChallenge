47825333156420d7bb2174030f2544241fa2f1cf
Alec Wixom <atwixom@gmail.com>
2023-05-10 10:14:03 -0600

    update

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ let phoneNumber = function (numbers) {
 }
 
 console.log(phoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+// function createPNumber (arr, ) {
+//     let template = `(801) xxx-xxxx`
+//     for (let i in arr) {
+//         template = template.replace(`x`, arr[i])
+//     }
+//     return template
+
+// }
+
+// console.log(createPNumber([4,4,4,5,5,5,5]));
